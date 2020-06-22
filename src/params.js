e36b31007d55ae4e79940ad89f8813b2d4722dcf
Rhafael Santos <rhafa99ss@gmail.com>
2020-06-22 20:07:36 -0300

    Initial commit

---
+++ src/params.js
@@ -0,0 +1,9 @@
+import { Dimensions } from 'react-native'
+
+const params ={
+    blockSize: 30,
+    borderSize: 5,
+    fontSize: 15,
+    headerRatio: 0.15, //Proporção do painel superior
+    difficultLevel: 0.1,
+}
